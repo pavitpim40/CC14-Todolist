3eb713876ffb80568d12c54bedcd8636b56270d9
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-24 17:41:14 +0700

    finish:todo header-time

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.scss';
+import './App.scss'; // Global
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { TodoContent } from './components/Todo/TodoContent';
