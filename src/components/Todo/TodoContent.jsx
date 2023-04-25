5ba4e9a79ae34aac606054e9dbf9782b929e038c
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-25 15:52:34 +0700

    finish:add-todo function

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,20 +1,34 @@
-import {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import { AddTodo } from './AddTodo';
 import { TodoHeader } from './TodoHeader';
 import { TodoLists } from './TodoLists';
-import mockData from '../../data/todos.json'
+import mockData from '../../data/todos.json';
 
 export function TodoContent() {
     // # Logic
-    const [todos,setTodos] = useState(mockData)
-   
+    const [todos, setTodos] = useState(mockData);
+
+    const handleAddTodo = () => {
+        // มี new todo
+        let newTodoObj = { id: uuidv4(), task: 'DoHW', status: false, due_date: '' };
+
+        // สร้าง state ใหม่
+        // update state โดย new state
+        // const newTodos = [newTodoObj, ...todos];
+        // setTodos(newTodos);
+
+        // update state โดย callback
+        setTodos(currentState=> [newTodoObj,...currentState])
+    };
 
     // # UI
     return (
         <main className='content'>
             <TodoHeader />
             <AddTodo />
-            <TodoLists  todos={todos}/>
+            <button onClick={handleAddTodo}>Test Add Todo</button>
+            <TodoLists todos={todos} />
         </main>
     );
 }
