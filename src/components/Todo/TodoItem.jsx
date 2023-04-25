8ba48a31b39a47bc77195225e0e59aa79312c21a
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-25 14:47:07 +0700

    finish : UI-interaction

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,21 +1,52 @@
 import styles from './TodoItem.module.scss';
+import { useState } from 'react';
 import { HiCheck, HiPencil, HiTrash } from 'react-icons/hi';
+import { TodoForm } from './TodoForm';
 
 export function TodoItem() {
+    // #1 : Logic,State
+    const [isCheck, setIsCheck] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
+
+    const handleToggleCheck = () => {
+        setIsCheck(!isCheck);
+    };
+
+	const handleOpenEditMode = () => {
+		setIsEdit(true)
+	}
+
+    const handleDeleteTodo = () => {
+        console.log('delete');
+    };
+
+    let checkboxStyle = isCheck ? styles.checkbox__icon__done : styles.checkbox__icon;
+    let taskStyle = isCheck ? styles.done : '';
+    // #2 : render
     return (
-        <li className={styles.todo__item__container}>
-            <div className={styles.checkbox__container}>
-                <HiCheck className={styles.checkbox__icon} />
-            </div>
-            <p className={styles.done}>{`task`}</p>
+        <>
+            {!isEdit ? (
+                <li className={styles.todo__item__container}>
+                    <div className={styles.checkbox__container} onClick={handleToggleCheck}>
+                        <HiCheck className={checkboxStyle} />
+                    </div>
+                    <p className={taskStyle}>{`OLD-TASK`}</p>
 
-            <div className={styles.edit__icon}>
-                <HiPencil />
-            </div>
+                    <div className={styles.edit__icon} onClick={handleOpenEditMode}>
+                        <HiPencil />
+                    </div>
 
-            <div className={styles.delete__icon}>
-                <HiTrash />
-            </div>
-        </li>
+                    <div className={styles.delete__icon} onClick={handleDeleteTodo}>
+                        <HiTrash />
+                    </div>
+                </li>
+            ) : (
+                <TodoForm 
+				submitText='Edit task'  
+				onSetIsShowForm={setIsEdit}
+				oldTask="old-task-name"
+				/>
+            )}
+        </>
     );
 }
