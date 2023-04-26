58c41e2bc4be533a2efaa0cc04e4e6db2c410eb0
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-26 14:52:27 +0700

    refactor:date-util with filter-list-fn

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { TodoContent } from './components/Todo/TodoContent';
 import mockData from './data/todos.json';
+import { getSevenDayRange } from './utils/DateUtils';
 
 function App() {
     const [todos, setTodos] = useState(mockData);
@@ -11,31 +12,18 @@ function App() {
     // Filter Todo
 
     const handleFilterLists = (index) => {
-        // idx == 0  : All
-        // idx == 1 : today
-        // idx == 2 : next 7
-        console.log('selected', index);
+        const [nowStr, nextSevenStr] = getSevenDayRange();
+        let filteredTodo = [...mockData]
 
         // FILTER LOGIC : Schema for fillter "2023-04-29" == YYYY-MM-DD
-        if (index === 0) setTodos(mockData); // mockData === All todo
-        else if (index === 1) {
-            const nowObj = new Date();
-            let nowStr = nowObj.toISOString().slice(0, 10); // "2023-04-26"
-            const filteredTodo = mockData.filter((todoObj) => todoObj.due_date === nowStr);
-            setTodos(filteredTodo);
+        if (index === 1) {
+             filteredTodo = mockData.filter((todoObj) => todoObj.due_date === nowStr);
         } else if (index === 2) {
-            const nowObj = new Date();
-            let nowStr = nowObj.toISOString().slice(0, 10);
-
-            const nextSevenDayObj = new Date(Date.now() + 6 * 24 * 60 * 60 * 1000);
-            let nextSevenDayStr = nextSevenDayObj.toISOString().slice(0, 10);
-
-            const filteredTodo = mockData.filter(
-                (todoObj) => todoObj.due_date >= nowStr && todoObj.due_date <= nextSevenDayStr
+             filteredTodo = mockData.filter(
+                (todoObj) => todoObj.due_date >= nowStr && todoObj.due_date <= nextSevenStr
             );
-            console.log(filteredTodo)
-            setTodos(filteredTodo)
         }
+        setTodos(filteredTodo);
     };
 
     return (
--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -7,3 +7,14 @@ export const getFormattedDate = (dateTime) => {
 
     return formattedDateStr;
 };
+
+// OUTPUT : ["YYYY-MM-DD:now", "YYYY-MM-DD:next-7-day"]
+export const getSevenDayRange = () => {
+    const nowObj = new Date();
+    let nowStr = nowObj.toISOString().slice(0, 10);
+
+    const nextSevenDayObj = new Date(Date.now() + 6 * 24 * 60 * 60 * 1000);
+    let nextSevenDayStr = nextSevenDayObj.toISOString().slice(0, 10);
+
+    return [nowStr, nextSevenDayStr];
+};
